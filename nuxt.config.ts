050c433195e7eccf76bb615f18d87a5558676b47
LoTwT <709937065@qq.com>
2022-04-20 23:27:48 +0800

    fix: color mode, persistant dark value in localStorage

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,7 +12,12 @@ export default defineNuxtConfig({
     ],
   },
 
-  modules: ["@vueuse/nuxt", "@unocss/nuxt", "@pinia/nuxt"],
+  modules: [
+    "@vueuse/nuxt",
+    "@unocss/nuxt",
+    "@pinia/nuxt",
+    "@nuxtjs/color-mode",
+  ],
 
   autoImports: {
     dirs: ["composables", "store"],
@@ -39,5 +44,9 @@ export default defineNuxtConfig({
         "px-4 py-1 rounded inline-block bg-teal-600 text-white cursor-pointer hover:bg-teal-700 disabled:cursor-default disabled:bg-gray-600 disabled:opacity-50",
       ],
     ],
+  },
+
+  colorMode: {
+    classSuffix: "",
   },
 })
